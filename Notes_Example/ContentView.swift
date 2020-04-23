bb42750498b277655ad26005842456b6b798de40
Jeremiah Kistler <jeremiahkistler@gmail.com>
2020-04-23 08:36:44 -0500

    Initial Commit

---
+++ Notes_Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notes_Example
+//
+//  Created by Jeremiah Kistler on 4/23/20.
+//  Copyright © 2020 Jeremiah Kistler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
